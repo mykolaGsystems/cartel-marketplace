2af6f1e2a7255202997f8b5f241ec68b9b7a2f26
mykolaGsystems <kolangavrish0001@ukr.net>
2023-09-29 15:44:45 +0200

    Beta announcement section added

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 import Navbarv2 from './Navbar_v2';
 import Footer from './Footer';
+import Box from '@mui/material/Box';
 
 const Layout = ({ children }) => {
   return (
     <div className="layout">
+        <Box className="layout-announcement-nav">
+          Our platform is currently in BETA environment
+        </Box>
       <header>
         <Navbarv2 />
       </header>
