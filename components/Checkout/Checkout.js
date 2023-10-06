32904bba10c4743c31e938f783cb76dfd44ada0b
mykolaGsystems <kolangavrish0001@ukr.net>
2023-10-07 01:41:39 +0200

    Functionality for verifying users purchase

--- components/Checkout/Checkout.js
+++ components/Checkout/Checkout.js
@@ -1,4 +1,4 @@
-import  React, { useEffect, useState } from "react";
+import  React, { useEffect, useState, useMemo, useRef } from "react";
 import CssBaseline from '@mui/material/CssBaseline';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -14,6 +14,8 @@ import Typography from '@mui/material/Typography';
 import AddressForm from './AddressForm';
 import PaymentForm from './PaymentForm';
 import Review from './Review';
+import CircularProgress from '@mui/material/CircularProgress';
+import {useNextReplaceUrl, nextReplaceUrl} from 'next-replace-url';
 
 import { encodeData }  from "../../lib/services";
 import { useCheckoutContext } from '../../context/CheckoutContext';
@@ -41,37 +43,51 @@ function getStepContent(step, delivery_options) {
 };
 
 export default function Checkout({ delivery_options }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isButtonLoading, setButtonLoading] = useState(false);
   const router = useRouter();
   const [activeStep, setActiveStep] = useState(0);
-  const { accountId, viewMethod, callMethods, getTransactionResult } = useNearContext();
+  const { accountId, viewMethod, callMethods, getState} = useNearContext();
   const { cartItems } = useStateContext();
   const [ hash, setHash ] = React.useState('');
+  const dataFetchedRef = useRef(false);
 
   const { firstName, lastName, email, 
           mobile, address1,  address2, 
           city, state, zip, country, updateEncrypted, encrypted, nearTotalPrice, deliveryRegion } = useCheckoutContext();
 
-  useEffect( async () => {
+  const verifyResult = async () => {
     const urlParams = new URLSearchParams(window.location.search);
     const txhash = urlParams.get("transactionHashes");
     if(txhash !== null){
-      router.push("/success");
-      // let result = await getTransactionResult(txhash);
-      // console.log("rez", result)
-      // if(result.transaction_outcome.outcome){
-      //   console.log("he")
-      // };
-      // console.log(result)
+      setIsLoading(true);
+      let txState = await getState(txhash, accountId);
+      let result = txState["transaction_outcome"]["outcome"]["status"]["SuccessReceiptId"];
+      let receipt_value = txState["receipts_outcome"]["3"]["outcome"]["logs"]["10"];
+      // Ask for the ID and display it on the receipt;
+      console.log("SuccessReceiptId : ", result);
+      
+      if (result !== "") {
+        setIsLoading(false)
+        router.push("/success");
+      }
     };
+  }
 
-  },[hash]);
+  useMemo( async () => {
+    if (dataFetchedRef.current) return;
+    dataFetchedRef.current = true;
+    verifyResult();
+    
+  }, [])
 
 
+  // useEffect( async () => {
+  //   c
+
+  // },[hash]);
 
   const nearPay = async () => { 
-
-    // let deposit = utils.format.parseNearAmount("1"); 
-    // let deposit = nearTotalPrice.toString()
 
     let items = []
 
@@ -164,58 +180,68 @@ export default function Checkout({ delivery_options }) {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
-        <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-          <Typography component="h1" variant="h4" align="center">
-            Checkout
-          </Typography>
-          <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
-            {steps.map((label) => (
-              <Step key={label}>
-                <StepLabel>{label}</StepLabel>
-              </Step>
-            ))}
-          </Stepper>
-          {activeStep === steps.length ? (
-            <React.Fragment>
-              <Typography variant="h5" gutterBottom>
-                Thank you for your order.
-              </Typography>
-              <Typography variant="subtitle1">
-                Your order number is #2001539. We have emailed your order
-                confirmation, and will send you an update when your order has
-                shipped.
-              </Typography>
-            </React.Fragment>
-          ) : (
-            <React.Fragment>
-              {getStepContent(activeStep, delivery_options)}
-              <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-                {activeStep !== 0 && (
-                  <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
-                    Back
-                  </Button>
-                )}
+      {isLoading ? (
+        <Box 
+          sx={{ display: 'flex', justifyContent: 'center',  alignItems: 'center', height: '20vh',  }}>
+          <CircularProgress />
+        </Box>
+      ) : (
+        <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+          <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+            <Typography component="h1" variant="h4" align="center">
+              Checkout
+            </Typography>
+            <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
+              {steps.map((label) => (
+                <Step key={label}>
+                  <StepLabel>{label}</StepLabel>
+                </Step>
+              ))}
+            </Stepper>
+            {activeStep === steps.length ? (
+              <React.Fragment>
+                <Typography variant="h5" gutterBottom>
+                  Thank you for your order.
+                </Typography>
+                <Typography variant="subtitle1">
+                  Your order number is #2001539. We have emailed your order
+                  confirmation, and will send you an update when your order has
+                  shipped.
+                </Typography>
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                {getStepContent(activeStep, delivery_options)}
+                <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                  {activeStep !== 0 && (
+                    <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
+                      Back
+                    </Button>
+                  )}
 
-                <Button
-                  variant="contained"
-                  onClick={() => {
-                    if(activeStep === steps.length - 1){
-                      nearPay();
-                    }else {
-                      handleNext();
-                    }
-               
-                  }}
-                  sx={{ mt: 3, ml: 1 }}
-                >
-                  {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
-                </Button>
-              </Box>
-            </React.Fragment>
-          )}
-        </Paper>
-      </Container>
+                  <Button
+                    variant="contained"
+                    disabled={isButtonLoading}
+                    onClick={() => {
+                      if(activeStep === steps.length - 1){
+                        if(encrypted !== '') {
+                          nearPay();
+                        }
+                      }else {
+                        handleNext();
+                      }
+                
+                    }}
+                    sx={{ mt: 3, ml: 1 }}
+                  >
+                    {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
+                  </Button>
+                </Box>
+              </React.Fragment>
+            )}
+          </Paper>
+        </Container>
+      )}
     </React.Fragment>
   );
 }
