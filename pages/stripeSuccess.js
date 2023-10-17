280960d13de42980fde186a6ac7e4c86f859a22e
mykolaGsystems <kolangavrish0001@ukr.net>
2023-10-17 13:59:15 +0200

    Page for stripe success

---
+++ pages/stripeSuccess.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { BsBagCheckFill } from 'react-icons/bs';
+
+import { useStateContext } from '../context/StateContext';
+import { runFireworks } from '../lib/utils';
+
+const Success = () => {
+  const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+  
+  useEffect(() => {
+    setCartItems([]);
+    setTotalPrice(0);
+    setTotalQuantities(0);
+  }, []);
+
+  return (
+    <div className="success-wrapper">
+      <div className="success">
+        <h2>Thank you for your order!</h2>
+        <p className="email-msg">We will be in touch with you shortly!</p>
+        <p className="description">
+          If you have any questions, please email
+          <a className="email" href="mailto:elcafecartel100@gmail.com">
+            elcafecartel100@gmail.com
+          </a>
+        </p>
+        <Link href="/">
+          <button type="button" width="300px" className="btn">
+            Continue Shopping
+          </button>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Success
